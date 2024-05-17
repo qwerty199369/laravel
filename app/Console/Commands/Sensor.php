89b06b3af9579010a0267bbde7c34c0f39015b67
qwerty199369 <32596673+qwerty199369@users.noreply.github.com>
2024-05-18 00:08:35 +0800

    Create Sensor.php

---
+++ app/Console/Commands/Sensor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Sensor extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sensor';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
