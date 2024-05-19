ad487171d2fda9fb760f5b2b6ebe8d44fe8362c8
qwerty199369 <32596673+qwerty199369@users.noreply.github.com>
2024-05-19 14:18:38 +0800

    up

--- app/Console/Commands/StructSensor.php
+++ app/Console/Commands/StructSensor.php
@@ -24,16 +24,16 @@ class StructSensor extends BaseBot
      */
     protected $description = 'Command description';
 
+    private int $ts;
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $this->info(substr(env('MINI_APPID_1'), 0, length: 3));
-        exit(1);
+        $this->ts = time();
 
         Assert::notNull($this->option('domain'));
-        Assert::notNull($this->option('sleep'));
 
         if (!Schema::hasTable('sensor')) {
             Schema::create('sensor', function (Blueprint $table) {
@@ -58,19 +58,21 @@ class StructSensor extends BaseBot
             true
         )['data']['list'];
 
-        $i = 0;
-
         foreach ($list as $item) {
+            if (time() - $this->ts > 600) {
+                break;
+            }
+
             $loc = "https://www.{$this->option('domain')}/brand/{$item['u']}.html";
 
-            $this->line("[$i][{$item['u']}]");
+            $this->line("[{$item['u']}]");
 
             $html = $this->getURLWithDB(
                 "$loc#{$item['u']}",
                 [
                     'Accept' => '*/*',
                     'Accept-Encoding' => 'gzip, deflate',
-                    'Host' => 'www.'.$this->option('domain'),
+                    'Host' => "www.{$this->option('domain')}",
                     'User-Agent' => "Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)",
                 ],
                 'sensor',
@@ -79,19 +81,29 @@ class StructSensor extends BaseBot
                 ]
             );
 
-            if (!$this->is_read_from_db) {
-                $i++;
-            }
-
-            if ($i > 100) {
-                break;
-            }
-
             if ($html === null) {
                 continue;
             }
 
             // $crawler = new Crawler($html);
+
+            preg_match('#company_id=(\d+)#', $html, $matches);
+            $company_id = $matches[1];
+
+            $productJson = $this->getURLWithDB(
+                "https://cgs.{$this->option('domain')}/v1/company/search-product?company_id=$company_id&page=1&pageSize=20#$company_id",
+                [
+                    'Accept' => 'application/json',
+                    'Accept-Encoding' => 'gzip, deflate',
+                    'Host' => "cgs.{$this->option('domain')}",
+                    'Referer' => "https://servicewechat.com/" . env('MINI_APPID_1') . "/30/page-frame.html",
+                    'User-Agent' => "Mozilla/5.0 (iPhone; CPU iPhone OS 17_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E287 MicroMessenger/8.0.45(0x1801512d) NetType/WIFI Language/zh_CN",
+                ],
+                'sensor',
+                [
+                    'kk' => "https://cgs.{$this->option('domain')}/v1/company/search-product?company_id=$company_id.1",
+                ]
+            );
         }
     }
 }
