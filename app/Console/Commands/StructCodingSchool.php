ef8aa94476c80b8d45ef7da3e6dbb7e9a6cf5be8
qwerty199369 <32596673+qwerty199369@users.noreply.github.com>
2024-05-31 23:52:51 +0800

    Create StructCodingSchool.php

---
+++ app/Console/Commands/StructCodingSchool.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StructCodingSchool extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'struct:coding-school';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
